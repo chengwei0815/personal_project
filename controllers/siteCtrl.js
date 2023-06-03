bfa90172aded8398f8e58708d570f39a131c1dcb
chengwei0815 <chengwei0815@gmail.com>
2023-06-03 16:39:47 -0400

    add login page and update navbar

--- controllers/siteCtrl.js
+++ controllers/siteCtrl.js
@@ -3,66 +3,74 @@ const siteData = require("../data/siteData");
 const passport = require('passport');
 
 module.exports = {
-    index: (request, response) => {
-        response.render('pages/index', {
-            name: siteData.userName,
-            copyrightYear: siteData.year,
-            signedIn:siteData.signedIn
-        });
-    },
-    register_get: (request, response) => {
-        response.render('pages/register', {
-            copyrightYear: siteData.year
-        });
-    },
-    register_post: (request, response) => {
-        User.register({ username: request.body.username }, request.body.password, (error, user) => {
-            if (error) {
-                console.log(error);
-                response.redirect('/register');
-            } else {
-                passport.authenticate('local')(request, response, () => {
-                    response.redirect('/login');
-                });
-            }
-        });
-    },
-    login_get: (request, response) => {
-        response.render('pages/login', {
-            copyrightYear: siteData.year
-        });
-    },
-    login_post: (request, response) => {
-        const { username, password } = request.body;
-        const user = new User({
-            username: username,
-            password: password
-        });
-        request.login(user, (error) => {
-            if (error) {
-                console.log(error);
-                response.redirect('/login');
-            } else {
-                passport.authenticate('local')(request, response, () => {
-                    response.redirect('/admin');
-                });
-            }
-        });
-    },
-    logout: (request, response) => {
-        request.logout(function (error) {
-            if (error) {
-                return next(error);
-            }
-                response, redirect('/');
-
-        })
-    },
-    google_get: passport.authenticate('google', { scope: ['openid', 'profile', 'email'] }),
-    google_redirect_get: [
-        passport.authenticate('google', { failureRedirect: '/login' }),
-        function (request, response) {
-            response.redirect('/admin');
+  index: (request, response) => {
+    response.render("pages/index", {
+      name: siteData.userName,
+      copyrightYear: siteData.year,
+      signedIn: siteData.signedIn,
+    });
+  },
+  register_get: (request, response) => {
+    response.render("pages/register", {
+      copyrightYear: siteData.year,
+    });
+  },
+  register_post: (request, response) => {
+    User.register(
+      { username: request.body.username },
+      request.body.password,
+      (error, user) => {
+        if (error) {
+          console.log(error);
+          response.redirect("/register");
+        } else {
+          passport.authenticate("local")(request, response, () => {
+            response.redirect("/login");
+          });
         }
-    ]
-}
+      }
+    );
+  },
+  login_get: (request, response) => {
+    response.render("pages/login", {
+      copyrightYear: siteData.year,
+    });
+  },
+  login_post: (request, response) => {
+    const { username, password } = request.body;
+    const user = new User({
+      username: username,
+      password: password,
+    });
+    request.login(user, (error) => {
+      if (error) {
+        console.log(error);
+        response.redirect("/login");
+      } else {
+        passport.authenticate("local")(request, response, () => {
+          response.redirect("/admin");
+        });
+      }
+    });
+  },
+  logout: (request, response) => {
+    // new code as of 6/2022 - the correct logout function
+    request.logout(function (err) {
+      // destroy the session for the user
+      if (err) {
+        return next(err);
+      }
+      // redirect back to the homepage
+      response.redirect("/");
+    });
+  },
+  google_get: passport.authenticate("google", {
+    scope: ["openid", "profile", "email"],
+  }),
+  google_redirect_get: [
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    function (request, response) {
+      response.redirect("/admin");
+    },
+  ],
+};
--- routes/siteRouter.js
+++ routes/siteRouter.js
@@ -10,11 +10,10 @@ router.route('/register').get(siteCtrl.register_get)
 router.route('/login').get(siteCtrl.login_get)
     .post(siteCtrl.login_post)
 
-router.route('logout').get(siteCtrl.logout)
+router.route('/logout').get(siteCtrl.logout)
 
 router.route('/auth/google').get(siteCtrl.google_get)
 
 router.route('/auth/google/admin').get(siteCtrl.google_redirect_get)
 
 module.exports = router;
-
